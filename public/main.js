b7a0698623a1c75a3d763c655fc1698e73371ab1
popcornto <jazmati6@gmail.com>
2024-12-09 06:00:01 +0100

    sdfg

--- public/main.js
+++ public/main.js
@@ -2,15 +2,13 @@ let maxTurns = 9;
 let playerTurn = true; // false for player 1 / true for player 2
 let matchPoint = false;
 let match = -1;
-player1Score = 0;
-player2Score = 0;
-tieScore = 0;
-
 
 const turnIndicator = document.getElementById("turn-indicator");
 const tickables = document.querySelectorAll(".tickable");
 const reset = document.getElementById("reset");
-
+const p1Score = document.getElementById("p1");
+const p2Score = document.getElementById("p2");
+const tieScore = document.getElementById("tie");
 
 const box00 = document.getElementById("00");
 const box10 = document.getElementById("10");
@@ -22,16 +20,33 @@ const box02 = document.getElementById("02");
 const box12 = document.getElementById("12");
 const box22 = document.getElementById("22");
 
+fetch("http://localhost:3000/score")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    updateScore(data.p1, data.p2, data.tie);
+  })
+  .catch((error) => console.error("Fetch error:", error));
+
 function reloadPage() {
   location.reload();
 }
 
 function checkBoxes(box1, box2, box3, value) {
   return (
-    box1.innerHTML === value &&
-    box2.innerHTML === value &&
-    box3.innerHTML === value
+    box1.innerText === value &&
+    box2.innerText === value &&
+    box3.innerText === value
   );
+}
+function updateScore(p1, p2, tie) {
+  p1Score.innerHTML = p1;
+  p2Score.innerHTML = p2;
+  tieScore.innerHTML = tie;
 }
 
 tickables.forEach(function (tickable) {
@@ -41,20 +56,17 @@ tickables.forEach(function (tickable) {
       //audio.play();
       if (playerTurn) {
         turnIndicator.textContent = "Player 2";
-        tickable.innerHTML = "X";
+        tickable.innerText = "X";
         playerTurn = false;
         checkWin();
         maxTurns--;
-        console.log(maxTurns);
       } else {
         turnIndicator.textContent = "Player 1";
         tickable.style.backgroundColor = "blanchedalmond";
-        tickable.innerHTML = "O";
+        tickable.innerText = "O";
         playerTurn = true;
         checkWin();
         maxTurns--;
-        console.log(maxTurns);
-        console.log(match + " :match");
       }
 
       tickable.classList.add("clicked");
@@ -66,190 +78,218 @@ tickables.forEach(function (tickable) {
         tickable.style.pointerEvents = "none";
       });
       if (match === 1) {
-        player2Score++;
-        turnIndicator.innerText = "Player 2 Won!";
-        reset.style.visibility = "visible";
-        reset.style.opacity = 1;
-        reset.onclick = reloadPage;
+        fetch("http://localhost:3000/addp2")
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
+            return response.json();
+          })
+          .then((data) => {
+            updateScore(data.p1, data.p2, data.tie);
+            turnIndicator.innerText = "Player 2 Won!";
+            reset.style.visibility = "visible";
+            reset.style.opacity = 1;
+            reset.onclick = reloadPage;
+          })
+          .catch((error) => console.error("Fetch error:", error));
       } else if (match === 2) {
-        player1Score++;
-        turnIndicator.textContent = "Player 1 Won!";
-        reset.style.visibility = "visible";
-        reset.style.opacity = 1;
-        reset.onclick = reloadPage;
+        fetch("http://localhost:3000/addp1")
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
+            return response.json();
+          })
+          .then((data) => {
+            updateScore(data.p1, data.p2, data.tie);
+            turnIndicator.textContent = "Player 1 Won!";
+            reset.style.visibility = "visible";
+            reset.style.opacity = 1;
+            reset.onclick = reloadPage;
+          })
+          .catch((error) => console.error("Fetch error:", error));
       }
     } else if (maxTurns === 0) {
-      tieScore++;
-      turnIndicator.textContent = "Its a Tie!";
-      reset.style.visibility = "visible";
-      reset.style.opacity = 1;
-      reset.onclick = reloadPage;
+      fetch("http://localhost:3000/addtie")
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          updateScore(data.p1, data.p2, data.tie);
+          turnIndicator.textContent = "Its a Tie!";
+          reset.style.visibility = "visible";
+          reset.style.opacity = 1;
+          reset.onclick = reloadPage;
+        })
+        .catch((error) => console.error("Fetch error:", error));
     }
-    console.log(match);
   });
 });
 
-
 function checkWin() {
   if (checkBoxes(box00, box10, box20, "X")) {
-    const cross = document.getElementById("cross0")
-    cross.style.visibility = "visible"
-    cross.style.transition = "transform 2s"
-    cross.classList.add("anime1")
+    const cross = document.getElementById("cross0");
+    cross.style.visibility = "visible";
+    cross.style.transition = "transform 2s";
+    cross.classList.add("anime1");
     const arr = document.getElementsByClassName("tickable");
     for (let i = 0; i < 9; i++) {
-      arr[i].style.cssText = "" 
+      arr[i].style.cssText = "";
     }
     match = 2;
   } else if (checkBoxes(box01, box11, box21, "X")) {
-    const cross = document.getElementById("cross1")
-    cross.style.visibility = "visible"
-    cross.style.transition = "transform 2s"
-    cross.classList.add("anime1")
+    const cross = document.getElementById("cross1");
+    cross.style.visibility = "visible";
+    cross.style.transition = "transform 2s";
+    cross.classList.add("anime1");
     const arr = document.getElementsByClassName("tickable");
     for (let i = 0; i < 9; i++) {
-      arr[i].style.cssText = "" 
+      arr[i].style.cssText = "";
     }
     match = 2;
   } else if (checkBoxes(box02, box12, box22, "X")) {
-    const cross = document.getElementById("cross2")
-    cross.style.visibility = "visible"
-    cross.style.transition = "transform 2s"
-    cross.classList.add("anime1")
+    const cross = document.getElementById("cross2");
+    cross.style.visibility = "visible";
+    cross.style.transition = "transform 2s";
+    cross.classList.add("anime1");
     const arr = document.getElementsByClassName("tickable");
     for (let i = 0; i < 9; i++) {
-      arr[i].style.cssText = "" 
+      arr[i].style.cssText = "";
     }
     match = 2;
   } else if (checkBoxes(box00, box01, box02, "X")) {
-    const cross = document.getElementById("cross3")
-    cross.style.visibility = "visible"
-    cross.style.transition = "transform 2s"
-    cross.classList.add("anime1")
+    const cross = document.getElementById("cross3");
+    cross.style.visibility = "visible";
+    cross.style.transition = "transform 2s";
+    cross.classList.add("anime1");
     const arr = document.getElementsByClassName("tickable");
     for (let i = 0; i < 9; i++) {
-      arr[i].style.cssText = "" 
+      arr[i].style.cssText = "";
     }
     match = 2;
   } else if (checkBoxes(box10, box11, box12, "X")) {
-    const cross = document.getElementById("cross4")
-    cross.style.visibility = "visible"
-    cross.style.transition = "transform 2s"
-    cross.classList.add("anime1")
+    const cross = document.getElementById("cross4");
+    cross.style.visibility = "visible";
+    cross.style.transition = "transform 2s";
+    cross.classList.add("anime1");
     const arr = document.getElementsByClassName("tickable");
     for (let i = 0; i < 9; i++) {
-      arr[i].style.cssText = "" 
+      arr[i].style.cssText = "";
     }
     match = 2;
   } else if (checkBoxes(box20, box21, box22, "X")) {
-    const cross = document.getElementById("cross5")
-    cross.style.visibility = "visible"
-    cross.style.transition = "transform 2s"
-    cross.classList.add("anime1")
+    const cross = document.getElementById("cross5");
+    cross.style.visibility = "visible";
+    cross.style.transition = "transform 2s";
+    cross.classList.add("anime1");
     const arr = document.getElementsByClassName("tickable");
     for (let i = 0; i < 9; i++) {
-      arr[i].style.cssText = "" 
+      arr[i].style.cssText = "";
     }
     match = 2;
   } else if (checkBoxes(box00, box11, box22, "X")) {
-    const cross = document.getElementById("cross7")
-    cross.style.visibility = "visible"
-    cross.style.transition = "transform 1s"
-    cross.classList.add("anime2")
+    const cross = document.getElementById("cross7");
+    cross.style.visibility = "visible";
+    cross.style.transition = "transform 1s";
+    cross.classList.add("anime2");
     const arr = document.getElementsByClassName("tickable");
     for (let i = 0; i < 9; i++) {
-      arr[i].style.cssText = "" 
+      arr[i].style.cssText = "";
     }
     match = 2;
   } else if (checkBoxes(box02, box11, box20, "X")) {
-    const cross = document.getElementById("cross6")
-    cross.style.visibility = "visible"
-    cross.style.transition = "transform 1s"
-    cross.classList.add("anime2")
+    const cross = document.getElementById("cross6");
+    cross.style.visibility = "visible";
+    cross.style.transition = "transform 1s";
+    cross.classList.add("anime2");
     const arr = document.getElementsByClassName("tickable");
     for (let i = 0; i < 9; i++) {
-      arr[i].style.cssText = "" 
+      arr[i].style.cssText = "";
     }
     match = 2;
   }
   if (checkBoxes(box00, box10, box20, "O")) {
-    const cross = document.getElementById("cross0")
-    cross.style.visibility = "visible"
-    cross.style.transition = "transform 2s"
-    cross.classList.add("anime1")
+    const cross = document.getElementById("cross0");
+    cross.style.visibility = "visible";
+    cross.style.transition = "transform 2s";
+    cross.classList.add("anime1");
     const arr = document.getElementsByClassName("tickable");
     for (let i = 0; i < 9; i++) {
-      arr[i].style.cssText = "" 
+      arr[i].style.cssText = "";
     }
     match = 1;
   } else if (checkBoxes(box01, box11, box21, "O")) {
-    const cross = document.getElementById("cross1")
-    cross.style.visibility = "visible"
-    cross.style.transition = "transform 2s"
-    cross.classList.add("anime1")
+    const cross = document.getElementById("cross1");
+    cross.style.visibility = "visible";
+    cross.style.transition = "transform 2s";
+    cross.classList.add("anime1");
     const arr = document.getElementsByClassName("tickable");
     for (let i = 0; i < 9; i++) {
-      arr[i].style.cssText = "" 
+      arr[i].style.cssText = "";
     }
     match = 1;
   } else if (checkBoxes(box02, box12, box22, "O")) {
-    const cross = document.getElementById("cross2")
-    cross.style.visibility = "visible"
-    cross.style.transition = "transform 2s"
-    cross.classList.add("anime1")
+    const cross = document.getElementById("cross2");
+    cross.style.visibility = "visible";
+    cross.style.transition = "transform 2s";
+    cross.classList.add("anime1");
     const arr = document.getElementsByClassName("tickable");
     for (let i = 0; i < 9; i++) {
-      arr[i].style.cssText = "" 
+      arr[i].style.cssText = "";
     }
     match = 1;
   } else if (checkBoxes(box00, box01, box02, "O")) {
-    const cross = document.getElementById("cross3")
-    cross.style.visibility = "visible"
-    cross.style.transition = "transform 2s"
-    cross.classList.add("anime1")
+    const cross = document.getElementById("cross3");
+    cross.style.visibility = "visible";
+    cross.style.transition = "transform 2s";
+    cross.classList.add("anime1");
     const arr = document.getElementsByClassName("tickable");
     for (let i = 0; i < 9; i++) {
-      arr[i].style.cssText = "" 
+      arr[i].style.cssText = "";
     }
     match = 1;
   } else if (checkBoxes(box10, box11, box12, "O")) {
-    const cross = document.getElementById("cross4")
-    cross.style.visibility = "visible"
-    cross.style.transition = "transform 2s"
-    cross.classList.add("anime1")
+    const cross = document.getElementById("cross4");
+    cross.style.visibility = "visible";
+    cross.style.transition = "transform 2s";
+    cross.classList.add("anime1");
     const arr = document.getElementsByClassName("tickable");
     for (let i = 0; i < 9; i++) {
-      arr[i].style.cssText = "" 
+      arr[i].style.cssText = "";
     }
     match = 1;
   } else if (checkBoxes(box20, box21, box22, "O")) {
-    const cross = document.getElementById("cross5")
-    cross.style.visibility = "visible"
-    cross.style.transition = "transform 2s"
-    cross.classList.add("anime1")
+    const cross = document.getElementById("cross5");
+    cross.style.visibility = "visible";
+    cross.style.transition = "transform 2s";
+    cross.classList.add("anime1");
     const arr = document.getElementsByClassName("tickable");
     for (let i = 0; i < 9; i++) {
-      arr[i].style.cssText = "" 
+      arr[i].style.cssText = "";
     }
     match = 1;
   } else if (checkBoxes(box00, box11, box22, "O")) {
-    const cross = document.getElementById("cross7")
-    cross.style.visibility = "visible"
-    cross.style.transition = "transform 2s"
-    cross.classList.add("anime2")
+    const cross = document.getElementById("cross7");
+    cross.style.visibility = "visible";
+    cross.style.transition = "transform 2s";
+    cross.classList.add("anime2");
     const arr = document.getElementsByClassName("tickable");
     for (let i = 0; i < 9; i++) {
-      arr[i].style.cssText = "" 
+      arr[i].style.cssText = "";
     }
     match = 1;
   } else if (checkBoxes(box02, box11, box20, "O")) {
-    const cross = document.getElementById("cross6")
-    cross.style.visibility = "visible"
-    cross.style.transition = "transform 2s"
-    cross.classList.add("anime2")
+    const cross = document.getElementById("cross6");
+    cross.style.visibility = "visible";
+    cross.style.transition = "transform 2s";
+    cross.classList.add("anime2");
     const arr = document.getElementsByClassName("tickable");
     for (let i = 0; i < 9; i++) {
-      arr[i].style.cssText = "" 
+      arr[i].style.cssText = "";
     }
     match = 1;
   }
--- server.js
+++ server.js
@@ -30,35 +30,60 @@ httpServer.listen(5000, ()=>{
 
 */
 
-import express from "express"
-import {dirname} from "path"
+import express from "express";
+import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { Axios } from "axios";
 
-
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = 3000;
 
-app.use("/", express.static('public'))
+let p1 = 0;
+let p2 = 0;
+let tie = 0;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use("/", express.static("public"));
 
 // OR use res.sendFile for a specific file
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+app.get("/reset", (req, res)=>{
+  p1 = p2 = tie = 0
+  res.send({ p1: p1, p2: p2, tie: tie });
+})
 
+app.get("/score", (req,res)=>{
+  res.send({ p1: p1, p2: p2, tie: tie });
+})
+app.get("/addp1", (req, res) => {
+  p1++;
+  res.send({ p1: p1, p2: p2, tie: tie });
+});
+app.get("/addp2", (req, res) => {
+  p2++;
+  res.send({ p1: p1, p2: p2, tie: tie });
+});
+app.get("/addtie", (req, res) => {
+  tie++;
+  res.send({ p1: p1, p2: p2, tie: tie });
+});
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "\\public\\index.html");
 });
 
-
-
-
-
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 
 /*
 
@@ -73,5 +98,4 @@ io.on("connection", (socket) => {
 
 io.listen(3000);
 
-*/ 
-
+*/
